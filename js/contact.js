733752dd040d3510bc786729ea747403253127cc
floatingbit23 <javisancab2003@gmail.com>
2024-05-18 15:08:01 +0200

    Changed Formspree layout

---
+++ js/contact.js
@@ -0,0 +1,32 @@
+document.addEventListener('DOMContentLoaded', () => {
+    const form = document.getElementById('contactForm');
+    const responseDiv = document.getElementById('form-response');
+
+    form.addEventListener('submit', async (event) => {
+        event.preventDefault(); // Evita que el formulario se envíe de la manera tradicional
+
+        const formData = new FormData(form);
+        
+        try {
+            const response = await fetch('https://formspree.io/f/xkndyqke', {
+                method: 'POST',
+                body: formData,
+                headers: {
+                    'Accept': 'application/json'
+                }
+            });
+
+            if (response.ok) {
+                form.reset(); // Limpia el formulario
+                responseDiv.style.display = 'block'; // Muestra el mensaje de respuesta
+                // responseDiv.textContent = 'Message sent!';
+            } else {
+                throw new Error('Network response was not ok.');
+            }
+        } catch (error) {
+            console.error('There was a problem with the fetch operation:', error);
+            responseDiv.style.display = 'block'; // Muestra el mensaje de error
+            responseDiv.textContent = 'There was a problem sending your message. Please try again later.';
+        }
+    });
+});
